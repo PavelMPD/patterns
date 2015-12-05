cc2235adf506e360b0ea30c384a61b786408850c
Pavel Matveev <pavelmpd82@gmail.com>
2015-12-05 11:03:40 +0300

    Add the function decorate with arguments

--- decorator/html_decorators.py
+++ decorator/html_decorators.py
@@ -12,12 +12,24 @@ def make_italic(fn):
     return wrapped
 
 
+def make_p(fn):
+    def wrapped(text):
+        return "<p>{}</p>".format(fn(text))
+
+    return wrapped
+
+
 if __name__ == "__main__":
     @make_bold
     @make_italic
     def module_name():
         return __name__
 
+    @make_p
+    def add_block(text):
+        return "block: {}".format(text)
 
     print("The module name:")
     print(module_name())
+    print(make_bold(module_name)())
+    print(add_block("new text"))
